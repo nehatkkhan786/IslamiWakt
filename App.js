d2f245b85dce02516b6b0a6f862f6970b547ba71
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2023-04-18 23:21:32 +0530

    initital commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location'
+
 
 export default function App() {
+
+  const [location, setLocation] = useState([])
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  const requestLocationPermission = async () =>{
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted'){
+      setErrorMsg('Permission To access location was denied');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location)
+    console.log(location)
+  }
+
+  useEffect(()=>{
+    requestLocationPermission();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
       <StatusBar style="auto" />
     </View>
   );
